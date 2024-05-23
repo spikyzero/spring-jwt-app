c9dc2368fa59e5c852a77fddd29e9db2c86b914f
spikyzero <nikeslavko0906@gmail.com>
2024-05-23 12:54:39 +0300

    Initial commit

---
+++ src/main/java/ua/com/SpringJwtAppApplication.java
@@ -0,0 +1,13 @@
+package ua.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtAppApplication.class, args);
+	}
+
+}
